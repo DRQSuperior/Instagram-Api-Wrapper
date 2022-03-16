84ea2f42b9aaba9d45df8945e2e511b5f03461a8
DRQSuperior <74627000+DRQSuperior@users.noreply.github.com>
2022-03-15 17:51:13 -0700

    A few more functions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const request = require('request');
+const json = require('json');
 
+//working
 function getAvatarByUrl(username) {
     return new Promise((resolve, reject) => {
         let url = `https://www.instagram.com/${username}/?__a=1`;
@@ -13,6 +15,7 @@ function getAvatarByUrl(username) {
     });
 }
 
+//working
 function getFollowers(username) {
     return new Promise((resolve, reject) => {
         let url = `https://www.instagram.com/${username}/?__a=1`;
@@ -26,6 +29,7 @@ function getFollowers(username) {
     });
 }
 
+//working
 function getFollowing(username) {
     return new Promise((resolve, reject) => {
         let url = `https://www.instagram.com/${username}/?__a=1`;
@@ -39,7 +43,8 @@ function getFollowing(username) {
     });
 }
 
-function getPosts(username) {
+//working
+function getPostsAmount(username) {
     return new Promise((resolve, reject) => {
         let url = `https://www.instagram.com/${username}/?__a=1`;
         request(url, (err, res, body) => {
@@ -52,6 +57,7 @@ function getPosts(username) {
     });
 }
 
+//working
 function isPrivate(username) {
     return new Promise((resolve, reject) => {
         let url = `https://www.instagram.com/${username}/?__a=1`;
@@ -65,6 +71,7 @@ function isPrivate(username) {
     });
 }
 
+//working
 function getBio(username) {
     return new Promise((resolve, reject) => {
         let url = `https://www.instagram.com/${username}/?__a=1`;
@@ -78,6 +85,7 @@ function getBio(username) {
     });
 }
 
+//working
 function getName(username) {
     return new Promise((resolve, reject) => {
         let url = `https://www.instagram.com/${username}/?__a=1`;
@@ -91,4 +99,95 @@ function getName(username) {
     });
 }
 
+//not sure if works
+function getLikes(username, postId) {
+    return new Promise((resolve, reject) => {
+        let url = `https://www.instagram.com/p/${postId}/?__a=1`;
+        request(url, (err, res, body) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(JSON.parse(body).graphql.shortcode_media.edge_media_preview_like.count);
+            }
+        });
+    });
+}
 
+//not sure if works
+function getComments(username, postId) {
+    return new Promise((resolve, reject) => {
+        let url = `https://www.instagram.com/p/${postId}/?__a=1`;
+        request(url, (err, res, body) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(JSON.parse(body).graphql.shortcode_media.edge_media_to_comment.count);
+            }
+        });
+    });
+}
+
+//not sure if works
+function getPostPicture(username, postId) {
+    return new Promise((resolve, reject) => {
+        let url = `https://www.instagram.com/p/${postId}/?__a=1`;
+        request(url, (err, res, body) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(JSON.parse(body).graphql.shortcode_media.display_url);
+            }
+        });
+    });
+}
+
+//not sure if works
+function getPostVideo(username, postId) {
+    return new Promise((resolve, reject) => {
+        let url = `https://www.instagram.com/p/${postId}/?__a=1`;
+        request(url, (err, res, body) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(JSON.parse(body).graphql.shortcode_media.video_url);
+            }
+        });
+    });
+}
+
+//not sure if works
+function getPostType(username, postId) {
+    return new Promise((resolve, reject) => {
+        let url = `https://www.instagram.com/p/${postId}/?__a=1`;
+        request(url, (err, res, body) => {
+            if (err) {
+                reject(err);
+            } else {
+                let type = JSON.parse(body).graphql.shortcode_media.is_video ? 'video' : 'picture';
+                resolve(type);
+            }
+        });
+    });
+}
+
+//not sure if works
+function getPostId(url) {
+    return new Promise((resolve, reject) => {
+        let id = url.split('/')[4];
+        resolve(id);
+    });
+}
+
+//not working
+function getAccountCreatedDate(username) {
+    return new Promise((resolve, reject) => {
+        let url = `https://www.instagram.com/${username}/?__a=1`;
+        request(url, (err, res, body) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(JSON.parse(body).graphql.user.edge_owner_to_timeline_media.edges[0].node.taken_at_timestamp);
+            }
+        });
+    });
+}
